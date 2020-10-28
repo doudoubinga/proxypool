de5965ecbfa53f0486062d8619792d9e4e27a6ac
doudoubinga <42226718+doudoubinga@users.noreply.github.com>
2020-10-28 13:27:07 +0800

    Update web_fanqiangdang.go

--- pkg/getter/web_fanqiangdang.go
+++ pkg/getter/web_fanqiangdang.go
@@ -7,8 +7,8 @@ import (
 	"sync"
 
 	"github.com/gocolly/colly"
-	"github.com/zu1k/proxypool/pkg/proxy"
-	"github.com/zu1k/proxypool/pkg/tool"
+	"github.com/doudoubinga/proxypool/pkg/proxy"
+	"github.com/doudoubinga/proxypool/pkg/tool"
 )
 
 func init() {

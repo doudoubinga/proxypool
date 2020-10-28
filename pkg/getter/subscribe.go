cfb7e851e9806a24ca9a32f558b67e12cad30347
doudoubinga <42226718+doudoubinga@users.noreply.github.com>
2020-10-28 13:26:21 +0800

    Update subscribe.go

--- pkg/getter/subscribe.go
+++ pkg/getter/subscribe.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/zu1k/proxypool/pkg/proxy"
-	"github.com/zu1k/proxypool/pkg/tool"
+	"github.com/doudoubinga/proxypool/pkg/proxy"
+	"github.com/doudoubinga/proxypool/pkg/tool"
 )
 
 func init() {

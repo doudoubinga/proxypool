3a8dce1548f218b4ceee63433b1c6b51415ced6f
doudoubinga <42226718+doudoubinga@users.noreply.github.com>
2020-10-28 13:18:38 +0800

    Update router.go

--- api/router.go
+++ api/router.go
@@ -11,10 +11,10 @@ import (
 	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
-	"github.com/zu1k/proxypool/config"
-	binhtml "github.com/zu1k/proxypool/internal/bindata/html"
-	C "github.com/zu1k/proxypool/internal/cache"
-	"github.com/zu1k/proxypool/pkg/provider"
+	"github.com/doudoubinga/proxypool/config"
+	binhtml "github.com/doudoubinga/proxypool/internal/bindata/html"
+	C "github.com/doudoubinga/proxypool/internal/cache"
+	"github.com/doudoubinga/proxypool/pkg/provider"
 )
 
 const version = "v0.3.10"
